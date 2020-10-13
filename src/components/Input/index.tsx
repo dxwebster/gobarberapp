52558e2259085d998c10002da139d1075c7e141a
dxwebster <adrianalimafm@gmail.com>
2020-10-13 18:22:57 -0300

    First commit

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,108 @@
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+} from 'react';
+
+// Formulários
+import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
+
+// Styled-Components
+import { Container, TextInput, Icon } from './styles';
+
+// Interfaces
+interface InputProps extends TextInputProps {
+  name: string;
+  icon: string;
+}
+
+interface inputValueReference {
+  value: string;
+}
+
+interface InputRef {
+  focus(): void;
+}
+
+// Component Input, que recebe todas as prorpriedades do input e também o ref
+const Input: React.RefForwardingComponent<InputRef, InputProps> = (
+  { name, icon, ...rest },
+  ref
+) => {
+  const { registerField, defaultValue = '', fieldName, error } = useField(name);
+
+  // Define as refs dos inputs
+  const inputValueRef = useRef<inputValueReference>({ value: defaultValue });
+  const inputElementRef = useRef<any>(null);
+
+  // Determina os estados de Foco e Preenchimmento
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    // se tiver algum valor no inputValueRef é true, se não tiver, é false
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
+  // pega a ref do input, e quando der focus, vai executar uma ação
+  // pega a ref do input, e quando der focus, vai executar uma ação
+  useImperativeHandle(ref, () => ({
+    focus() {
+      inputElementRef.current.focus();
+    },
+  }));
+
+  // Para lidar com as mudanças do Input
+  useEffect(() => {
+    registerField<string>({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+      // O vai acontecer com o input quando receber um novo valor
+      setValue(ref: any, value) {
+        inputValueRef.current.value = value;
+        inputElementRef.current.seNativeProps({ text: value });
+      },
+      // O que vai acontecer com o input quando o unform limpar
+      clearValue() {
+        inputValueRef.current.value = '';
+        inputElementRef.current.clear();
+      },
+    });
+  }, [fieldName, registerField]);
+
+  // Retorno do component
+  return (
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon
+        name={icon}
+        size={20}
+        color={isFocused || isFilled ? '#ff9000' : '#666360'}
+      />
+
+      <TextInput
+        ref={inputElementRef}
+        placeholderTextColor="#666360"
+        defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        onChangeText={(value) => {
+          inputValueRef.current.value = value;
+        }}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default forwardRef(Input);
---
+++ src/pages/Signin/index.tsx
@@ -0,0 +1,159 @@
+import React, { useCallback, useRef } from 'react';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert,
+} from 'react-native';
+
+import { useAuth } from '../../hooks/auth';
+
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import logoImg from '../../assets/logo.png';
+
+import {
+  Container,
+  Title,
+  ForgotPassword,
+  ForgotPasswordText,
+  CreateAccountButton,
+  CreateAccountButtonText,
+} from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SignIn: React.FC = () => {
+  // Referências para acessar um objeto e manipulá-lo
+  const formRef = useRef<FormHandles>(null); // referência do formulário
+  const passwordInputRef = useRef<TextInput>(null); // referência do input de password
+
+  // Navegação
+  const navigation = useNavigation();
+
+  const { signIn } = useAuth();
+
+  // Função que dispara no submit
+  const handleSignIn = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório')
+            .email('Digite um email válido'),
+          password: Yup.string().required('Senha obrigatória'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        // se o erro for gerado pelo Yupi, retorna o erro
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+        // senão, vai disparar um alert do react-native
+        Alert.alert(
+          'Erro na autenticação',
+          'Ocorreu um  erro ao fazer login, cheque as credenciais'
+        );
+      }
+    },
+    [signIn]
+  );
+
+  // Retorno da página
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logoImg} />
+
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
+
+            <Form ref={formRef} onSubmit={handleSignIn}>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                name="email"
+                icon="mail"
+                placeholder="Email"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
+              />
+
+              <Input
+                ref={passwordInputRef}
+                name="password"
+                icon="lock"
+                placeholder="Senha"
+                secureTextEntry
+                returnKeyType="send"
+                onSubmitEditing={() => {
+                  formRef.current?.submitForm();
+                }}
+              />
+
+              <Button
+                onPress={() => {
+                  formRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
+
+            <ForgotPassword onPress={() => {}}>
+              <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+            </ForgotPassword>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton onPress={() => navigation.navigate('SignUp')}>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+      </CreateAccountButton>
+    </>
+  );
+};
+
+export default SignIn;
